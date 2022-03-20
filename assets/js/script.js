f1c4b0a6f04b2c80ccf08ca21a7edf2015d996e9
george-downer40 <george.downer40@gmail.com>
2022-03-20 19:19:54 +0000

    add remaining functionality for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,7 +93,10 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, 'multiply'];
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
-    } else {
+    } else if (operator === '/') {
+        return [operand1 / operand2, 'division'];
+    }     
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -145,7 +148,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand1').textContent = (operand1 * operand2);
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '/';
 }
